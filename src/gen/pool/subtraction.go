de39a93c93a0fc66a70838f640d75e1edc80b217
mouguangyi <mouguangyi@ztgame.com>
2019-09-03 15:03:35 +0800

    Not subtract self since it's too easy.

--- src/gen/pool/subtraction.go
+++ src/gen/pool/subtraction.go
@@ -19,7 +19,7 @@ type subtraction struct {
 func (s *subtraction) init(min int, max int) {
 	s.items = make([]gen.IFactor, 0)
 	for i := max; i >= min; i-- {
-		for j := i; j >= min; j-- {
+		for j := i - 1; j >= min; j-- {
 			s.items = append(s.items, gen.NewFactor(i).Append(gen.NewFactor(j), operator.NewSubtract()))
 		}
 	}
