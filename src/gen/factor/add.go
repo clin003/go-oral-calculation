8e59ab71d39e3862526397c5bcbaca4928ce6dec
mouguangyi <mouguangyi@ztgame.com>
2019-09-11 18:09:06 +0800

    Refine character.

--- src/gen/factor/add.go
+++ src/gen/factor/add.go
@@ -27,19 +27,19 @@ func (f *add) Format(t gen.FormatType, withResult bool) string {
 
 	if t == gen.PH_LEFT {
 		if Poss(0.5) {
-			formula += fmt.Sprintf("%s + %s", f.a.Format(t, false), f.b.Format(gen.PH_NONE, false))
+			formula += fmt.Sprintf("%s ＋ %s", f.a.Format(t, false), f.b.Format(gen.PH_NONE, false))
 		} else {
-			formula += fmt.Sprintf("%s + %s", f.a.Format(gen.PH_NONE, false), f.b.Format(t, false))
+			formula += fmt.Sprintf("%s ＋ %s", f.a.Format(gen.PH_NONE, false), f.b.Format(t, false))
 		}
 	} else {
-		formula += fmt.Sprintf("%s + %s", f.a.Format(gen.PH_NONE, false), f.b.Format(gen.PH_NONE, false))
+		formula += fmt.Sprintf("%s ＋ %s", f.a.Format(gen.PH_NONE, false), f.b.Format(gen.PH_NONE, false))
 	}
 
 	if withResult {
 		if t == gen.PH_RIGHT {
-			formula += " = (   )"
+			formula += " ＝ (   )"
 		} else {
-			formula += fmt.Sprintf(" = %2d", f.Result())
+			formula += fmt.Sprintf(" ＝ %2d", f.Result())
 		}
 	}
 
--- src/gen/factor/sub.go
+++ src/gen/factor/sub.go
@@ -27,20 +27,20 @@ func (f *sub) Format(t gen.FormatType, withResult bool) string {
 
 	if t == gen.PH_LEFT {
 		if Poss(0.5) {
-			formula += fmt.Sprintf("%s - %s", f.a.Format(t, false), f.b.Format(gen.PH_NONE, false))
+			formula += fmt.Sprintf("%s － %s", f.a.Format(t, false), f.b.Format(gen.PH_NONE, false))
 		} else {
-			formula += fmt.Sprintf("%s - %s", f.a.Format(gen.PH_NONE, false), f.b.Format(t, false))
+			formula += fmt.Sprintf("%s － %s", f.a.Format(gen.PH_NONE, false), f.b.Format(t, false))
 		}
 	} else {
 
-		formula += fmt.Sprintf("%s - %s", f.a.Format(gen.PH_NONE, false), f.b.Format(gen.PH_NONE, false))
+		formula += fmt.Sprintf("%s － %s", f.a.Format(gen.PH_NONE, false), f.b.Format(gen.PH_NONE, false))
 	}
 
 	if withResult {
 		if t == gen.PH_RIGHT {
-			formula += " = (   )"
+			formula += " ＝ (   )"
 		} else {
-			formula += fmt.Sprintf(" = %2d", f.Result())
+			formula += fmt.Sprintf(" ＝ %2d", f.Result())
 		}
 	}
 
